406b412c7f17518fa1db9d5079d8f919a2964429
Charlie Guan <charliegdev@gmail.com>
2024-12-12 22:05:06 -0800

    Add some tests to the index controller of products_controller_test.rb

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select '.product-image', 3
+    assert_select '.title', 3
+    assert_select '.description', 3
+    assert_select '.action-menu', 3
   end
 
   test "should get new" do
