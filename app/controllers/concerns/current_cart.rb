b54d8625e7d36aad576c04dd0099dc8d7254cb49
Charlie Guan <charliegdev@gmail.com>
2024-12-22 23:19:16 -0800

    Unit tests for line item quantities

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+# Handle the session[:cart_id] setup
 module CurrentCart
   private
+
   def set_cart
     @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -18,8 +18,8 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should create cart' do
-    assert_difference('Cart.count') do
-      post carts_url, params: { cart: {  } }
+    assert_difference('Cart.count', 1) do
+      post carts_url, params: { cart: {} }
     end
 
     assert_redirected_to cart_url(Cart.last)
@@ -36,7 +36,7 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update cart' do
-    patch cart_url(@cart), params: { cart: {  } }
+    patch cart_url(@cart), params: { cart: {} }
     assert_redirected_to cart_url(@cart)
   end
 
@@ -49,5 +49,28 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to store_index_url
+  end
+
+  test 'should handle unique products added to cart' do
+    # Create a cart
+    post line_items_url, params: { product_id: products(:ruby).id }
+    post line_items_url, params: { product_id: products(:two).id }
+
+    # Add a new item to that cart
+    @cart = Cart.find(session[:cart_id])
+    assert_equal @cart.line_items.count, 2
+    assert_equal @cart.line_items.find_by(product_id: products(:ruby).id).quantity, 1
+    assert_equal @cart.line_items.find_by(product_id: products(:two).id).quantity, 1
+  end
+
+  test 'should handle duplicate products added to cart' do
+    # Create a cart
+    post line_items_url, params: { product_id: products(:ruby).id }
+    post line_items_url, params: { product_id: products(:ruby).id }
+
+    # Add a new item to that cart
+    @cart = Cart.find(session[:cart_id])
+    assert_equal @cart.line_items.count, 1
+    assert_equal @cart.line_items.find_by(product_id: products(:ruby).id).quantity, 2
   end
 end
