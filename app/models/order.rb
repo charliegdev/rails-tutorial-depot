60fe8ae2077a88c33c09c78110290dc1d553b8ea
Charlie Guan <charliegdev@gmail.com>
2025-01-03 06:35:38 -0800

    Modify migration script, and run migration

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  address    :text
+#  email      :string
+#  name       :string
+#  pay_type   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Order < ApplicationRecord
+  enum pay_type: {
+    'Check' => 0,
+    'Credit Card' => 1,
+    'Purchase Order' => 2
+  }
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  address    :text
+#  email      :string
+#  name       :string
+#  pay_type   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
