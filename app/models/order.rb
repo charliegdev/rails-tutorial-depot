7b3413661ccacb3031eb39a3cc887e3d932e5631
Charlie Guan <charliegdev@gmail.com>
2025-01-04 11:35:17 -0800

    Verify fields must exist before submitting

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,4 +16,7 @@ class Order < ApplicationRecord
     'Credit Card' => 1,
     'Purchase Order' => 2
   }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 end
