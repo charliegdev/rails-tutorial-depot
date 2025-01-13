d7cf95b342b2518d2b6d90980ae25d22d24714bd
Charlie Guan <charliegdev@gmail.com>
2025-01-13 07:01:31 -0800

    Test: when a user selects a payment type, the correct form fields show up.

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,5 +1,41 @@
 require 'application_system_test_case'
 
 class OrdersTest < ApplicationSystemTestCase
+  test 'check dynamic fields' do
+    visit store_index_url
 
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
+    assert has_no_field? 'Routing number'
+    assert has_no_field? 'Account number'
+    assert has_no_field? 'Credit card number'
+    assert has_no_field? 'Expiration date'
+    assert has_no_field? 'Po number'
+
+    select 'Check', from: 'Payment type'
+
+    assert has_field? 'Routing number'
+    assert has_field? 'Account number'
+    assert has_no_field? 'Credit card number'
+    assert has_no_field? 'Expiration date'
+    assert has_no_field? 'Po number'
+
+    select 'Credit Card', from: 'Payment type'
+
+    assert has_no_field? 'Routing number'
+    assert has_no_field? 'Account number'
+    assert has_field? 'Credit card number'
+    assert has_field? 'Expiration date'
+    assert has_no_field? 'Po number'
+
+    select 'Purchase Order', from: 'Payment type'
+
+    assert has_no_field? 'Routing number'
+    assert has_no_field? 'Account number'
+    assert has_no_field? 'Credit card number'
+    assert has_no_field? 'Expiration date'
+    assert has_field? 'Po number'
+  end
 end
